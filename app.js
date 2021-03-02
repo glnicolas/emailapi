2f2981f5560ad256c166df8647fe785556111acc
Nicolas Garcia Lira <nico17.ngl@gmail.com>
2021-03-02 04:21:36 -0600

    Allowed CORS

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 require('dotenv').config();
 
-
-
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 const port = process.env.PORT || 3000;;
 
 var mail = nodemailer.createTransport({
